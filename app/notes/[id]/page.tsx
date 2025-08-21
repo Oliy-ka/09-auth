cba2be9bc4cbf1eb4f272e7416f0158d26140f5b
oliyka <olja.x.06@gmail.com>
2025-08-21 22:35:39 +0300

    change place of file

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -4,7 +4,7 @@ import {
   QueryClient,
 } from "@tanstack/react-query";
 import { fetchNoteById } from "@/lib/api";
-import NoteDetailsClient from "../NoteDetails.client";
+import NoteDetailsClient from "./NoteDetails.client";
 
 interface NotePageProps {
   params: { id: string };
