d7ab5f5814d8edf06e399c25c77d69e7dbac2d79
oliyka <olja.x.06@gmail.com>
2025-09-14 17:55:02 +0300

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { checkServerSession } from "@/lib/api/serverApi";
+import { cookies } from "next/headers";
 
 const privateRoutes = ["/profile", "/notes"];
 const publicRoutes = ["/sign-in", "/sign-up"];
@@ -7,9 +8,9 @@ const publicRoutes = ["/sign-in", "/sign-up"];
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
-  const accessToken = request.cookies.get("accessToken")?.value;
-  const refreshToken = request.cookies.get("refreshToken")?.value;
-
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get("accessToken")?.value;
+  const refreshToken = cookieStore.get("refreshToken")?.value;
 
   const isPrivateRoute = privateRoutes.some((route) =>
     pathname.startsWith(route)
@@ -26,7 +27,7 @@ export async function middleware(request: NextRequest) {
     try {
       const res = await checkServerSession();
 
-      if (res.data.success) {
+      if (res.status === 200) {
         return NextResponse.next();
       } else {
         return NextResponse.redirect(new URL("/sign-in", request.url));
